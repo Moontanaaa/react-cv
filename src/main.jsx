dbb77b138db9f792ed5ddc51f664a65d1bf1f07a
Moontanaaa <montanafabio@icloud.com>
2024-08-10 16:56:03 +0200

    add servicePage

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import HomePage from './pages/Home/homePage';
 import BlogPage from './pages/blog/blogPage';
+import ServicePage from './pages/service/servicePage';
 import Realisation from './pages/realisation/realisationPage';
 import Header from './components/header/headerComponent';
 import Contact from './pages/contact/contactPage';
@@ -14,7 +15,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter />
-    <Realisation />
+    <ServicePage />
     <BrowserRouter />
   </StrictMode>,
 )
