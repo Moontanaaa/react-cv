9f8d9e6b22fbc0947054df224ef20b69ed962d02
Moontanaaa <montanafabio@icloud.com>
2024-08-10 14:07:28 +0200

    add homepage

---
+++ src/pages/Home/homePage.jsx
@@ -0,0 +1,21 @@
+import './homePage.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div class="responsive-container">
+        <div class="background-image">
+          <div class="centered">
+            <h1>Bonjour, je suis John Doe</h1>
+            <h2>Développeur web full stack</h2>
+            <button>En savoir plus</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
