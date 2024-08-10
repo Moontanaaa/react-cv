34066132bdc00ae89c90beacc0e67836bf68dd00
Moontanaaa <montanafabio@icloud.com>
2024-08-10 20:09:16 +0200

    final

--- src/components/header/headerComponent.jsx
+++ src/components/header/headerComponent.jsx
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid ms-4">
-        <a className="navbar-brand" href="#">JOHN DOE</a>
+        <a className="navbar-brand" href="/">JOHN DOE</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
